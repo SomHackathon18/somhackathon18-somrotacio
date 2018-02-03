8b3a1de59175357dae13bb744530fcb42ee5f0d0
Marc Vila <marcvilagomez@gmail.com>
2018-02-03 11:05:52 +0100

    First version Android app

---
+++ app/app/src/main/java/com/sommobilitat/hackathon/somhackathon2018/DashboardFragment.java
@@ -0,0 +1,32 @@
+package com.sommobilitat.hackathon.somhackathon2018;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class DashboardFragment extends Fragment {
+    private View rootView;
+    private TextView title;
+
+    public static DashboardFragment newInstance() {
+        return new DashboardFragment();
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        setUpElements();
+        title.setText(getString(R.string.title_dashboard));
+
+        return rootView;
+    }
+
+    private void setUpElements() {
+        title = (TextView) rootView.findViewById(R.id.title_tv);
+    }
+}
