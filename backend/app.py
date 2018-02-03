e5aeb126643a9307627a133717ac37280027c972
Sergio <sergio.paredes@est.fib.upc.edu>
2018-02-03 11:07:42 +0100

    Create endpoint for post parkings

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+
+def validate_parking(parking):
+    pass
+
+
+@app.route('/parking', methods=['POST'])
+def parking_post():
+    parking = request.get_json()
+    validate_parking(parking)
+    return parking, 201
+
 
 if __name__ == '__main__':
     app.run()
