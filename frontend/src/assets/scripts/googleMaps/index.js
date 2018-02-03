e3a7b575ea6ecbe92d3c3870c9d9f88b3f6812e7
Jordi Casanovas <jordi.casanovas@fib.upc.edu>
2018-02-03 14:34:35 +0100

    exportar a fichero maps

--- frontend/src/assets/scripts/googleMaps/index.js
+++ frontend/src/assets/scripts/googleMaps/index.js
@@ -1,78 +1,19 @@
 import * as $ from 'jquery';
-import loadGoogleMapsAPI  from 'load-google-maps-api';
 
 export default (function () {
-  if ($('#google-map').length > 0) {
-    loadGoogleMapsAPI({
-      key: 'AIzaSyDW8td30_gj6sGXjiMU0ALeMu1SDEwUnEA',
-    }).then(() => {
-      const latitude  = 26.8206;
-      const longitude = 30.8025;
-      const mapZoom   = 5;
-      const { google }    = window;
+  if ($('#mapid').length > 0) {
+    $.getJSON("http://178.62.30.127/cid", function (data) {
 
-      const mapOptions = {
-        center    : new google.maps.LatLng(latitude, longitude),
-        zoom      : mapZoom,
-        mapTypeId : google.maps.MapTypeId.ROADMAP,
-        styles: [{
-          'featureType': 'landscape',
-          'stylers': [
-            { 'hue'        : '#FFBB00' },
-            { 'saturation' : 43.400000000000006 },
-            { 'lightness'  : 37.599999999999994 },
-            { 'gamma'      : 1 },
-          ],
-        }, {
-          'featureType': 'road.highway',
-          'stylers': [
-            { 'hue'        : '#FFC200' },
-            { 'saturation' : -61.8 },
-            { 'lightness'  : 45.599999999999994 },
-            { 'gamma'      : 1 },
-          ],
-        }, {
-          'featureType': 'road.arterial',
-          'stylers': [
-            { 'hue'        : '#FF0300' },
-            { 'saturation' : -100 },
-            { 'lightness'  : 51.19999999999999 },
-            { 'gamma'      : 1 },
-          ],
-        }, {
-          'featureType': 'road.local',
-          'stylers': [
-            { 'hue'        : '#FF0300' },
-            { 'saturation' : -100 },
-            { 'lightness'  : 52 },
-            { 'gamma'      : 1 },
-          ],
-        }, {
-          'featureType': 'water',
-          'stylers': [
-            { 'hue'        : '#0078FF' },
-            { 'saturation' : -13.200000000000003 },
-            { 'lightness'  : 2.4000000000000057 },
-            { 'gamma'      : 1 },
-          ],
-        }, {
-          'featureType': 'poi',
-          'stylers': [
-            { 'hue'        : '#00FF6A' },
-            { 'saturation' : -1.0989010989011234 },
-            { 'lightness'  : 11.200000000000017 },
-            { 'gamma'      : 1 },
-          ],
-        }],
-      };
+      console.log(data);
+      var mymap = L.map('mapid').setView([41.538113, 2.444741], 13);
 
-      const map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-
-      new google.maps.Marker({
-        map,
-        position : new google.maps.LatLng(latitude, longitude),
-        visible  : true,
-      });
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.streets'
+      }).addTo(mymap);
     });
   }
 }())
