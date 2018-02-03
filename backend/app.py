ccecd06508ab8c9a935c5978eb86b80148e496a7
Sergio <sergio.paredes@est.fib.upc.edu>
2018-02-03 13:39:12 +0100

    Add CORS support

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, logging, Response, jsonify
+from flask_cors import CORS
 
 from persistence import Persistence
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
