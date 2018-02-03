3feb681dc998c3869377722c5e221c8edabdc7cb
Jordi Casanovas <jordi.casanovas@fib.upc.edu>
2018-02-03 19:31:01 +0100

    pot canviar entre marcadors i poligons

--- frontend/src/assets/scripts/markerMap/index.js
+++ frontend/src/assets/scripts/markerMap/index.js
@@ -5,33 +5,55 @@ export default (function () {
     $.getJSON("http://178.62.30.127/cid", function (data) {
 
       console.log(data);
-      var mymap = L.map('mapid').setView([41.538113, 2.444741], 13);
+
       var polygons = [];
-      //var marker = [];
+      var marker = [];
+
       for (var key in data) {
 
-        //var lat = data[key].LAT;
-        //var lng = data[key].LNG;
-        //lat = lat.replace(",", ".");
-        //lng = lng.replace(",", ".");
-
-        //marker[key] = L.marker([lat, lng]).addTo(mymap);
+        var lat = data[key].LAT;
+        var lng = data[key].LNG;
 
         var polygon = data[key].WKT.replace("POLYGON ((", "").replace("))", "");
         polygon = polygon.split(", ").map(function(p){return p.split(" ").reverse();});
-        polygons[key] = L.polygon(polygon).addTo(mymap);
+
+        marker[key] = L.marker([lat, lng]);
+        polygons[key] = L.polygon(polygon);
+
+        marker[key].bindPopup("Zona <b>"+data[key].ID+"</b>" +
+          "<br>Alçada <b>"+data[key].ALCADA+"</b>" +
+          "<br>Amplada <b>"+data[key].AMPLADA+"</b>" +
+          "<br>Area <b>"+data[key].AREA+"</b>");
         polygons[key].bindPopup("Zona <b>"+data[key].ID+"</b>" +
                                 "<br>Alçada <b>"+data[key].ALCADA+"</b>" +
                                 "<br>Amplada <b>"+data[key].AMPLADA+"</b>" +
                                 "<br>Area <b>"+data[key].AREA+"</b>");
       }
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+
+      var polygonsLayer = L.layerGroup(polygons);
+      var markersLayer = L.layerGroup(marker)
+
+      var city = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
         id: 'mapbox.streets'
-      }).addTo(mymap);
+      });
+      var mymap = L.map('mapid', {
+        center: [41.538113, 2.444741],
+        zoom: 14,
+        layers: [city, markersLayer]
+        }
+      );
+      var baseMaps = {
+        "Ciutat": city
+      }
+      var overlayMaps = {
+        "Marcadors": markersLayer,
+        "Polígons": polygonsLayer
+      }
+      L.control.layers(baseMaps, overlayMaps).addTo(mymap);
     });
   }
 }())
